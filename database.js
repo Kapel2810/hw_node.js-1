09d6390034dedba8ed6344a3e10fdac5c096d3e0
Kapel2810 <84678338+Kapel2810@users.noreply.github.com>
2022-02-24 14:32:00 +0200

    done

---
+++ database.js
@@ -0,0 +1,12 @@
+const espressoClubCoffee = [
+    { id: 1, type: 'Arabeska', strength: "3", price: "50"},
+    { id: 2, type: 'Arabeska Strong', strength: "5", price: "50"},
+    { id: 3, type: 'Colombian', strength: "4", price: "70"},
+    { id: 4, type: 'Ethiopia', strength: "3", price: "70"},
+    { id: 5, type: 'Costa-Rica', strength: "4", price: "65"},
+    { id: 6, type: 'Costa-Rica Vanilla', strength: "3", price: "75"},
+    { id: 7, type: 'Turkish', strength: "4", price: "40"}
+]
+export {
+    espressoClubCoffee
+}
--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-
 import express from "express"
+import generalSettings from "./config.js"
+import { espressoClubCoffee } from "./database.js"
+
+// console.log(generalSettings)
 const app= express()
-app.use(express.json())
+app.use(express.json()) 
 
 
+app.get(`${generalSettings.baseUrl}`, (req,res)=> {
+    res.send("main")
+})
 
+app.get(`${generalSettings.baseUrl}/coffee`, (req,res)=> {
+    res.send(espressoClubCoffee)
+})
 
+app.get(`${generalSettings.baseUrl}/coffee/:id`, (req,res)=> {
+    let id = +req.params.id
+    const requestedCoffeeType = 
+    espressoClubCoffee.find(coffeeT => coffeeT.id === id)
+    res.send(requestedCoffeeType)
+   
+})
 
+app.post(`${generalSettings.baseUrl}/coffee`, (req,res)=> {
+    const coffeeTypeToAdd = req.body
+    const lastId = espressoClubCoffee[espressoClubCoffee.length -1].id
+    let newCoffeeType = {
+        ...coffeeTypeToAdd,
+        id: lastId+1
+    }
 
-
-
-
+    espressoClubCoffee.push(newCoffeeType)
+    res.send(newCoffeeType)
+})
 
 
 app.listen(5001)
